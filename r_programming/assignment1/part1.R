8486382341cb879bd4a05f567a3bf0e7148b76fa
Kevin Yeung <kyeung@thoughtworks.com>
2015-09-12 23:38:48 +0800

    Add as_data_frame that returns the combined csv files into one data frame

--- r_programming/assignment1/part1.R
+++ r_programming/assignment1/part1.R
@@ -2,4 +2,12 @@ full_paths <- function(directory, ids) {
     sapply(ids, function(id) { paste(directory, id, sep="/") })
 }
 
+as_data_frame <- function(files) {
+    all_data_frames <- lapply(files, function(file) { read.csv(file) })
+    df <- data.frame()
+    for (i in 1:length(all_data_frames)) {
+        df <- rbind(df, all_data_frames[[i]])
+    }
+    df
+}
 
